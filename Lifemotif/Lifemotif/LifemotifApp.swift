cb281188d215799706c400d28534e47192dfbf55
Theodor Christensen <theodor.christensen02@gmail.com>
2024-02-02 10:46:15 +0100

    Update README.md

---
+++ Lifemotif/Lifemotif/LifemotifApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifemotifApp.swift
+//  Lifemotif
+//
+//  Created by Theodor Christensen on 2024-02-20.
+//
+
+import SwiftUI
+
+@main
+struct LifemotifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
