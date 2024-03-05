b716f1d5869f7129302b7c4ef2f84374f5657369
ObidAljabi <158558620+ObidAljabi@users.noreply.github.com>
2024-03-05 11:28:04 +0100

    Update main.swift

--- main.swift
+++ main.swift
@@ -5,20 +5,4 @@
 //  Created by Theodor Christensen on 2024-02-20.
 //
 
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, universe!")
-        }
-        .padding()
-    }
-}
-
-#Preview {
-    ContentView()
-}
+print("hello");
